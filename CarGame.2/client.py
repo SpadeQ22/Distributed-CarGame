bcd52c27ee0540a63af71ae4d61a86ab28463b03
Omar Mabrouk <67244800+SpadeQ22@users.noreply.github.com>
2023-06-17 04:06:22 +0300

    edited channel.py to be simpler and added redis-cluster configuration

---
+++ CarGame.2/client.py
@@ -0,0 +1,65 @@
+
+import sys
+import threading
+import time
+
+import channel
+import socket
+import pickle
+
+# REDIS_PRIMARY_LOCATION = 'localhost'
+# REDIS_PRIMARY_PORT = 32771
+#
+# REDIS_SECONDARY_LOCATION = 'localhost'
+# REDIS_SECONDARY_PORT = 30777
+
+class Client(socket.socket):
+
+    def __init__(self, serverLoc, port):
+        super().__init__(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = serverLoc
+        self.port = port
+        self.connect((self.server, self.port))
+        print(f"client connected to server at {self.server}:{self.port}")
+        self.chan = channel.Channel()
+        self.uuid = self.chan.join('client')
+        self.chan.subgroup('server')
+
+
+
+    def sendMsg(self):
+        message = {
+            'type': 'POST',
+            'groupId': 'client',
+            'uuid': self.uuid,
+            'message': {
+                'user': 'Omar0',
+                'body': 'hello world'
+            }
+        }
+        self.sendall(pickle.dumps(message))
+        # print(self.chan.recvFrom(senderSet=self.server, timeout=100))
+        return
+
+    def getLatestMessages(self):
+        while True:
+            time.sleep(1)
+            servers = self.chan.subgroup('server')
+            self.chan.join('client')
+            print(self.chan.recvFrom(servers, timeout=100))
+
+
+if __name__ == '__main__':
+    cli = Client("localhost", 1234)
+    thread = threading.Thread(target=cli.getLatestMessages, args=())
+    thread.start()
+    try:
+        while True:
+            try:
+                cli.sendMsg()
+                time.sleep(10)
+            except KeyboardInterrupt:
+                break
+    except KeyboardInterrupt:
+        cli.close()
+        sys.exit(0)
--- CarGame.2/server-chat.py
+++ CarGame.2/server-chat.py
@@ -1,8 +1,8 @@
 import pickle
 import socket
 import threading
-import os
 import channel
+
 
 class Server(socket.socket):
     def __init__(self, host, port):
@@ -11,9 +11,8 @@ class Server(socket.socket):
         self.port = port
         self.bind((self.host, self.port))
         print(f"server started at  {self.host, self.port}")
-        self.chan = channel.Channel(0, flush=True)
-        self.chan.bind(0)
-        self.server = self.chan.join('server')
+        self.chan = channel.Channel()
+        self.chan.join('server')
         self.workers = []
 
     def run(self):
@@ -29,27 +28,39 @@ class Server(socket.socket):
         finally:
             if self:
                 self.close()
+                self.chan.leave('server')
             for t in self.workers:
                 t.join()
 
     def handleConnection(self, conn, client_addr):
         conn.setblocking(1)
-        conn.settimeout(240)
+        conn.settimeout(60)
+        groupId = ""
+        uuid = ""
         try:
             while conn:
                 data = conn.recv(2048)
                 message_with_meta = pickle.loads(data)
                 print(f"received data from {client_addr}: {message_with_meta}")
+                groupId = message_with_meta['groupId']
+                uuid = message_with_meta['uuid']
                 if message_with_meta['type'] == 'POST':
                     self.chan.join(message_with_meta['groupId'])
                     clients = self.chan.subgroup(message_with_meta['groupId'])
                     self.chan.sendTo(destinationSet=clients, message=message_with_meta['message'])
         except socket.timeout:
             conn.close()
+            print("time")
         except EOFError:
             conn.close()
-
-
+            print("EOF")
+        except ConnectionError:
+            conn.close()
+            print("ConErr")
+        finally:
+            self.chan.force_leave(groupId, uuid)
+            self.workers.remove(threading.current_thread())
+            return
 
 
 if __name__ == '__main__':
