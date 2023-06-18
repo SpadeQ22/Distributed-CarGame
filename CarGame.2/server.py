1d0fe887002214c8adafe91b7e13537c254dc1ce
Abdel Rahman <19p9131@eng.asu.edu.eg>
2023-06-18 21:23:37 +0300

    Minor bug fixes

--- CarGame.2/server.py
+++ CarGame.2/server.py
@@ -1,45 +1,81 @@
 from dnetwork import dNetwork
 from _thread import *
 from car import Car
+import time
+from backup import save_game_state, get_game_state
+import threading
 
-ip = "127.0.0.1"
-port=5555
+lock = threading.Lock()
 
-def client_handler(id, client,car):
-    global game_network,p
+flag=True
+ip = "localhost"
+port = 55555
+last_call_time = time.time()
+players_connected = 0
+
+def checkpoint(pn):
+    global last_call_time
+    current_time = time.time()
+    if current_time - last_call_time >= 30:
+        last_call_time = current_time
+        save_game_state(1, pn)
+
+
+game_network = dNetwork(ip, port)
+connection = game_network.host()
+
+num_of_players = 2
+connection.listen(num_of_players)
+
+p = []
+messages = []
+
+
+def wait_for_client():
+    global players_connected
+    while True:
+        if flag:
+            client, _ = connection.accept()
+
+        if players_connected != num_of_players:
+            id = int(game_network.recv(client))
+            if id==-1:
+                id=players_connected
+            car = Car(players_connected)
+            p.append(car.to_dict())
+            save_game_state(1,p)
+            game_network.send(client, car)
+            start_new_thread(client_handler, (id, client))
+            players_connected += 1
+        else:
+            client.close()
+
+def client_quit(id):
+    global players_connected
+    client, _ = connection.accept()
+    reconn_id = int(game_network.recv(client))
+    if reconn_id == -1:
+        reconn_id = id
+    car=Car.retrieve_car(p[id])
     game_network.send(client, car)
+    start_new_thread(client_handler, (reconn_id, client))
+    players_connected += 1
+
+def client_handler(id, client):
+    global game_network, p,players_connected
+    p = get_game_state(1)
 
     while True:
         try:
-            if id==0:
-                p[0]=game_network.recv(client)[0]
-                game_network.send(client, [p[1], messages])
-                if len(p[0]["message"])>1:
-                    messages.append(p[0]["message"])
-            elif id==1:
-                game_network.send(client, [p[0], messages])
-                p[1] = game_network.recv(client)[0]
-                if len(p[1]["message"])>1:
-                    messages.append(p[1]["message"])
+            # checkpoint(p)
+            list = [x for x in p if x["id"] != id]
+            p[id] = game_network.recv(client)[0]
+            game_network.send(client, [list, messages])
+            if len(p[id]["message"]) > 1:
+                messages.append(p[id]["message"])
+                print(messages)
         except:
+            client_quit(id)
             quit()
 
-players_connected=0
-
-game_network=dNetwork(ip, port)
-connection=game_network.host()
-
-connection.listen(3)
-players=[Car(0),Car(1)]
-p=[Car(0).to_dict(""),Car(1).to_dict("")]
-
-messages=[]
-
-while True:
-    client,_=connection.accept()
-    if players_connected!=2 :
-        start_new_thread(client_handler,(players_connected,client,players[players_connected]))
-        messages.append(f"Player {players_connected} is connected")
-        players_connected+=1
-    else:
-        client.close()
+wait_for_client()
