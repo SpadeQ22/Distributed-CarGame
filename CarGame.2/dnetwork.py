93c1449e3c0d0ddf311b198ff628460deacd9196
Zakaria Madkour <82716623+Zakaria-Madkour@users.noreply.github.com>
2023-06-18 17:13:24 +0300

    Apply some subtle changes

--- CarGame.2/dnetwork.py
+++ CarGame.2/dnetwork.py
@@ -1,5 +1,6 @@
 import socket
 import pickle
+import subprocess
 
 class dNetwork:
     def __init__(self,ip,port):
@@ -7,17 +8,36 @@ class dNetwork:
         self.port=port
         self.connection=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-    def connect(self):
+    def check_server(self):
+        cmd = f"netstat -aon | findstr :{self.port}"
+        output = subprocess.getoutput(cmd)
+        return f"LISTENING" in output
+
+    def connect(self,session):
         try:
             self.connection.connect((self.ip,self.port))
             print("Successfully connected")
+            self.send(self.connection, [-1,session])
             return self.connection
         except:
             print("Server unreachable")
 
+
+    def reconnect(self,id,session):
+        try:
+            self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connection.connect((self.ip,self.port))
+            self.send(self.connection,[id,session])
+            print("Successfully reconnected")
+            return self.connection
+        except:
+            print("Server unreachable")
+            return -1
+
     def host(self):
         try:
             self.connection.bind((self.ip,self.port))
+            self.serverconnected=True
             print("Server started")
             return self.connection
         except:
@@ -33,5 +53,4 @@ class dNetwork:
     def recv(self,connection):
         data_length=connection.recv(8)
         data=connection.recv(int(data_length))
-
         return pickle.loads(data)
