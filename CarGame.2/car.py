73186fa25e4d968fa0f40a22ec3b45a6d785761f
mahmoudelnashar <84504283+mahmoudelnashar@users.noreply.github.com>
2023-06-18 19:02:02 +0300

    Update car.py

--- CarGame.2/car.py
+++ CarGame.2/car.py
@@ -1,9 +1,10 @@
 import random
 class Car:
-
     def __init__(self,id):
         self.display_width = 1200
         self.display_height = 600
+        self.x_coordinate=0
+        self.y_coordinate=0
         self.x_coordinate = None
         self.y_coordinate = None
         self.high_score = 0
@@ -17,10 +18,15 @@ class Car:
         self.width = 49
         self.connected=False
 
-    def to_dict(self,message):
-        return {'id': self.id, 'x_coordinate': self.x_coordinate, 'y_coordinate': self.y_coordinate,'high_score': self.high_score,
-                'message': message}
+    def to_dict(self):
+        return {'id': self.id, 'x_coordinate': self.x_coordinate, 'y_coordinate': self.y_coordinate,'high_score': self.high_score}
 
-
+    @staticmethod
+    def retrieve_car(car):
+        new_car=Car(car["id"])
+        new_car.x_coordinate=car["x_coordinate"]
+        new_car.y_coordinate = car["y_coordinate"]
+        new_car.high_score = car["high_score"]
+        return new_car
 
 
